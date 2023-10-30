2f012b907e82eaa88dc4e9a9145ef5da7bc5e927
SourishM15 <131424980+SourishM15@users.noreply.github.com>
2023-10-30 00:37:24 -0500

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,18 @@
+"""
+Baconian Cipher
+By: Sourish and Deion
+"""
+
+encrypt = ""
+Alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+Baconion = ['aaaaa', 'aaaab', 'aaaba', 'aaabb', 'aabaa', 'aabab', 'aabba', 'aabbb', 'abaaa', 'abaab', 'ababa', 'ababb', 'abbaa', 'abbab', 'abbba', 'abbbb', 'baaaa', 'baaab', 'baaba', 'baabb', 'babaa', 'babab', 'babba', 'babbb', 'bbaaa', 'bbaab']
+print("Type a word of your choice:")
+UI = input().upper()
+charArr = [char for char in UI]
+for x in range(len(charArr)):
+  for y in range(len(Alpha)):
+    if charArr[x] == Alpha[y]:
+      encrypt += Baconion[y]
+print(encrypt)
+      
+
